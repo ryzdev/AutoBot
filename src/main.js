dc4322c136847a996c4c24b2d43b48b8d8cc754b
Alan Russell <alan.russell@autotrader.co.uk>
2014-12-18 15:02:31 +0000

    enemies semi added

--- src/main.js
+++ src/main.js
@@ -11,19 +11,27 @@ var Q = Quintus({ audioSupported: [ 'mp3' ] })
 //Q.gravityY = 9.8;
  
 var objectFiles = [
-  './src/sprites/player',
-  './src/audio'
+    './src/sprites/player',
+    './src/sprites/enemies',
+    './src/audio'
 ];
  
 require(objectFiles, function () {
     //creating scene on stage
+
+    var enemyAssets = [
+        ["SecurityGuard", {x: 50, y: 50, asset: "enemies/security_guard.png"}]
+    ];
+
     Q.scene("firstStreet",function(stage) {
-    var background = new Q.TileLayer({ dataAsset: 'firstStreet.tmx', layerIndex: 0, sheet: 'tiles', tileW: 70, tileH: 70, type: Q.SPRITE_NONE });  //Q.SPRITE_NONE nocoll
-    stage.insert(background);
-    stage.collisionLayer(new Q.TileLayer({ dataAsset: 'firstStreet.tmx', layerIndex:1,  sheet: 'tiles', tileW: 70, tileH: 70 }));
-    // add player onto scene
-    var player = stage.insert(new Q.Player());
-    stage.add("viewport").follow(player,{x: true, y: true},{minX: 0, maxX: background.p.w, minY: 0, maxY: background.p.h});
+        var background = new Q.TileLayer({ dataAsset: 'firstStreet.tmx', layerIndex: 0, sheet: 'tiles', tileW: 70, tileH: 70, type: Q.SPRITE_NONE });  //Q.SPRITE_NONE nocoll
+        stage.insert(background);
+        stage.collisionLayer(new Q.TileLayer({ dataAsset: 'firstStreet.tmx', layerIndex:1,  sheet: 'tiles', tileW: 70, tileH: 70 }));
+        // add player onto scene
+        var player = stage.insert(new Q.Player());
+        stage.add("viewport").follow(player,{x: true, y: true},{minX: 0, maxX: background.p.w, minY: 0, maxY: background.p.h});
+
+        stage.loadAssets(enemyAssets);
     });
 
     Q.scene("endGame",function(stage) {
