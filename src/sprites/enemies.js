7cc00a46d08592990b381e15ff6e21a91386851e
Alan Russell <alan.russell@autotrader.co.uk>
2014-12-19 09:50:42 +0000

    harder first level

--- src/sprites/enemies.js
+++ src/sprites/enemies.js
@@ -112,4 +112,41 @@ require([], function () {
             }
         }
     });
+
+
+    Q.Sprite.extend("VerticalEnemyToAvoid", {
+        init: function(p) {
+            this._super(p, {vy: -150, rangeY: 100, gravity: 0 });
+            this.add("2d");
+
+            this.p.initialY = this.p.y;
+
+            this.on("bump.left,bump.right,bump.bottom",function(collision) {
+                if(collision.obj.isA("Player")) {
+                    Q.stageScene("endGame",1, { label: "Game Over" });
+                    collision.obj.destroy();
+                } else {
+                    this.p.vy = -150;
+                }
+            });
+            this.on("bump.top",function(collision) {
+                if(collision.obj.isA("Player")) {
+                    collision.obj.p.vy = -100;
+                    this.destroy();
+                } else {
+                    console.log('1')
+                }
+            });
+        },
+
+        step: function(dt) {
+            if(this.p.y - this.p.initialY >= this.p.rangeY && this.p.vy > 0) {
+                this.p.vy = -this.p.vy;
+            }
+            else if(-this.p.y + this.p.initialY >= this.p.rangeY && this.p.vy < 0) {
+                this.p.vy = -this.p.vy;
+            }
+        }
+
+    });
 });
