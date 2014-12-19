034fb5460d1cefb690ce36f3ae71156de1aa70eb
Alan Russell <alan.russell@autotrader.co.uk>
2014-12-19 11:16:16 +0000

    recommendation stars

---
+++ src/sprites/recommendation.js
@@ -0,0 +1,15 @@
+require([], function () {
+    Q.Sprite.extend("Recommendation", {
+        init: function(p) {
+            this._super(p, {});
+            this.add("2d");
+            this.on("bump.left,bump.right,bump.top,bump.bottom",function(collision) {
+                if(collision.obj.isA("Player")) {
+                    Q.state.inc("recommendations", 1);
+                    //kill enemy
+                    this.destroy();
+                }
+            });
+        }
+    });
+});
