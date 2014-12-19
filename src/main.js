896acfcfc54c16d72ca532c2efa35515c7928230
Ryz Jenvey <ryszard.jenvey@autotrader.co.uk>
2014-12-19 09:59:04 +0000

    changed ebay to falling wheel

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ require(objectFiles, function () {
         ["EnemyToAvoid", {x: 1000, y: 2050, asset: "enemies/security-guard.png"}],
         ["EnemyToAvoid", {x: 500, y: 2050, asset: "enemies/security-guard.png"}],
         ["VerticalEnemyToAvoid", {x: 700, y: 1950, asset: "enemies/security-guard.png"}],
-        ["EnemyToKill", {x: 280, y: 910, asset: "enemies/ebay.png"}],
+        ["EnemyToKill", {x: 280, y: 910, asset: "enemies/wheel.png"}],
         ["Badge", {x: 450, y: 1800, asset: "badge.png"}],
 
         ["BadgeGuard", {x: 350, y: 1800, asset: "enemies/security-guard.png"}]
@@ -110,7 +110,7 @@ require(objectFiles, function () {
 
 
 
-    Q.load("tiles_map.png, autobot.png, firstStreet.tmx, enemies/security-guard.png, enemies/ebay.png, badge.png", function() { //creating stage (layer)
+    Q.load("tiles_map.png, autobot.png, firstStreet.tmx, enemies/security-guard.png, enemies/wheel.png, badge.png", function() { //creating stage (layer)
         Q.sheet("tiles","tiles_map.png", { tilew: 70, tileh: 70});
         Q.stageScene("firstStreet");
         Q.stageScene("gameStats",1);
